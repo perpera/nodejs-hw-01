e10054fe588572511232e965f889b089cc091057
perpera <poalina@icloud.com>
2025-02-22 22:27:56 +0200

    added countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+
+    return `Кількість Ваших контактів - ${contacts.length}`;
+  } catch (error) {
+    console.error('Сталася помилка при спробі порахувати контакти', error);
+  }
+};
 
 console.log(await countContacts());
