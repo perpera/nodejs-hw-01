9508069ec359df592105eab421c1f068cbf90900
perpera <poalina@icloud.com>
2025-02-22 22:14:11 +0200

    added getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,17 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      return [];
+    } else {
+      return contacts;
+    }
+  } catch (error) {
+    console.error('Сталася помилка при спробі отримати контакти', error);
+  }
+};
 
 console.log(await getAllContacts());
