13dceddc03bdd9db518855fecc427cceed92e18c
perpera <poalina@icloud.com>
2025-02-22 22:04:34 +0200

    added addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContact = createFakeContact();
+
+    contacts.push(newContact);
+
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error('Сталася помилка при спробі додати новий контакт', error);
+  }
+};
 
 addOneContact();
