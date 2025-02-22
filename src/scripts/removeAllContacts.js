0ed99fc5008a17d387159252163fd5e9173864db
perpera <poalina@icloud.com>
2025-02-22 22:35:52 +0200

    added removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,18 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+      return;
+    }
+
+    await writeContacts([]);
+  } catch (error) {
+    console.error('Сталася помилка при спробі видалення всіх контактів', error);
+  }
+};
 
 removeAllContacts();
